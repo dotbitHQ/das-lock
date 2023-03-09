c707fcbc5f83b32b6727067ebb68d5b3a5f0f179
hellojason3 <hellojason328@gmail.com>
2023-03-09 07:52:21 +0000

    fix: support typeid and update test2_so_list

--- dispatch.c
+++ dispatch.c
@@ -554,7 +554,7 @@ int main() {
 	uint8_t code_buffer[128 * 1024] __attribute__((aligned(RISCV_PGSIZE)));
 	uint64_t consumed_size = 0;
 	void *handle = NULL;
-	uint8_t hash_type = 0;
+	uint8_t hash_type = 1;
 	ret = ckb_dlopen2(code_so, hash_type, code_buffer, 128 * 1024, &handle, &consumed_size);
 	SIMPLE_ASSERT(CKB_SUCCESS);
 	debug_print("after ckb_dlopen2");
